fbb8aba3a055db8fd27121054af3226a0fcc47d5
Nithin Kumar <nkdiyasys@gmail.com>
2020-04-24 10:49:10 +0530

    c1

---
+++ PipelineiOSTest/PipelineiOSTest/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  PipelineiOSTest
+//
+//  Created by Nithin Kumar on 4/24/20.
+//  Copyright © 2020 Diya Systems. All rights reserved.
+//
+
+import UIKit
+import PDFKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var pdfView: PDFView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        pdfViewFunc()
+    }
+     func pdfViewFunc() {
+               
+               
+               guard let path = Bundle.main.url(forResource: "sample", withExtension: "pdf") else { return }
+
+               if let document = PDFDocument(url: path) {
+                   pdfView.document = document
+               }
+               
+           }
+
+        @IBAction func tapMeClicked(_ sender: Any) {
+        let alert = UIAlertController.init(title: "Test", message: "Test alert", preferredStyle: .alert)
+
+              alert.addAction(UIAlertAction.init(title: "Done", style: .default, handler: { (action) in
+                  alert.dismiss(animated: true, completion: nil)
+              }))
+
+              self.present(alert, animated: true, completion: nil)
+          
+        }
+    }
---
+++ PipelineiOSTest/PipelineiOSTestTests/PipelineiOSTestTests.swift
@@ -0,0 +1,45 @@
+//
+//  PipelineiOSTestTests.swift
+//  PipelineiOSTestTests
+//
+//  Created by Nithin Kumar on 4/24/20.
+//  Copyright © 2020 Diya Systems. All rights reserved.
+//
+
+import XCTest
+@testable import PipelineiOSTest
+
+class PipelineiOSTestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testIncrement() {
+            let counter = Counter()
+
+            counter.increment()
+
+            XCTAssertEqual(counter.count, 1)
+        }
+
+        func testIncrementBy() {
+            let counter = Counter()
+
+            counter.increment(by: 10)
+
+            XCTAssertEqual(counter.count, 10)
+        }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
